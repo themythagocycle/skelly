76a3ecf47896ef8c012dd474204ae817507a78f3
Nathan Austin <themythagocycle@gmail.com>
2015-10-28 19:57:23 -0400

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, controllers: { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'dashboard/index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
