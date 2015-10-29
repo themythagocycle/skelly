dca5310de1615d076591325686f262534103e80f
Nathan Austin <themythagocycle@gmail.com>
2015-10-28 20:26:18 -0400

    added fixtures and tests

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'get authenticated dashboard' do
+    @user = users(:dick)
+    sign_in @user
+    get :index
+    assert_response :success
+  end
+
+  test 'get unauthenticated dashboard' do
+    get :index
+    assert_response :redirect
+  end
 end
